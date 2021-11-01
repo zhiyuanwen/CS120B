45e35e229567febcd04905c22a0890d1e4a786f5
zhiyuanwen <72575234+zhiyuanwen@users.noreply.github.com>
2021-10-31 20:45:34 -0700

    Add files via upload

--- zwen012_lab9_part2.c
+++ zwen012_lab9_part2.c
@@ -10,9 +10,45 @@
  */
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
+
+volatile unsigned char TimerFlag = 0;
+
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
+void TimerOn() {
+    TCCR1B = 0x0B;
+    OCR1A = 125;
+    TIMSK1 = 0x02;
+    TCNT1 = 0;
+    _avr_timer_cntcurr = _avr_timer_M;
+    SREG |= 0x80;
+}
+
+void TimerOff() {
+    TCCR1B = 0x00;
+}
+
+void TimerISR() {
+    TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect) {
+    _avr_timer_cntcurr--;
+    if(_avr_timer_cntcurr == 0) {
+        TimerISR();
+        _avr_timer_cntcurr = _avr_timer_M;
+    }
+}
+
+void TimerSet(unsigned long M) {
+    _avr_timer_M = M;
+    _avr_timer_cntcurr = _avr_timer_M;
+}
 
 void set_PWM(double frequency) {
     static double current_frequency;
@@ -104,6 +140,8 @@ void increment_Button() {
 int main(void) {
     DDRA = 0x00; PORTA = 0xFF;
     DDRB = 0xFF; PORTB = 0x00;
+    TimerSet(1000);
+    TimerOn();
     PWM_on();
     while(1) {
         increment_Button();
@@ -133,6 +171,8 @@ int main(void) {
                 set_PWM(523.25);
             }
         }
+        while(!TimerFlag);
+        TimerFlag = 0;
     }
     
     PWM_off();
