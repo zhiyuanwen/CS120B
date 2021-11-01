788d187cf8b1a8fd0af62d0203e890009249ee25
zhiyuanwen <72575234+zhiyuanwen@users.noreply.github.com>
2021-10-31 20:02:57 -0700

    Add files via upload

---
+++ zwen012_lab9_part2.c
@@ -0,0 +1,98 @@
+/*	Author: Zhiyuan Wen
+ *	Lab Section: 021
+ *	Assignment: Lab #9  Exercise #2
+ *	Exercise Description:
+ *	
+ *	I acknowledge all content contained herein, excluding template 
+ * 	or example code, is my own original work.
+ *
+ *	Demo Link: 
+ */
+
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+void set_PWM(double frequency) {
+    static double current_frequency;
+    if(frequency != current_frequency) {
+        if(!frequency) { TCCR3B &= 0x08; }
+        else { TCCR3B |= 0x03; }
+        if(frequency < 0.954) { OCR3A = 0xFFFF; }
+        else if(frequency > 31250) { OCR3A = 0x0000; }
+        else { OCR3A = (short)(8000000 / (128 * frequency)) - 1; }
+        TCNT3 = 0;
+        current_frequency = frequency;
+    }
+}
+
+void PWM_on() {
+    TCCR3A = (1 << COM3A0);
+    TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+    set_PWM(0);
+}
+
+void PWM_off() {
+    TCCR3A = 0x00;
+    TCCR3B = 0x00;
+}
+
+int main(void) {
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
+    PWM_on();
+    unsigned char noteNum = 0;
+    unsigned bool offOn = true;
+    while(1) {
+        if(PINA & 0x01) {
+            if(offOn) {
+                set_PWM(0);
+                offOn = true;
+            }
+            else {
+                offOn = false;
+            }
+        }
+        else if(PINA & 0x02) {
+            if(noteNum > 0) {
+                noteNum -= 1;
+            }
+        }
+        else if(PINA & 0x04) {
+            if(notNum < 7) {
+                noteNum += 1;
+            }
+        }
+
+        if(!offOn) {
+            if(noteNum == 0) {
+                set_PWM(261.63);
+            }
+            else if(noteNum == 1) {
+                set_PWM(293.66);
+            }
+            else if(noteNum == 2) {
+                set_PWM(329.63);
+            }
+            else if(noteNum == 3) {
+                set_PWM(349.23);
+            }
+            else if(noteNum == 4) {
+                set_PWM(392.00);
+            }
+            else if(noteNum == 5) {
+                set_PWM(440.00);
+            }
+            else if(noteNum == 6) {
+                set_PWM(493.88);
+            }
+            else if(noteNum == 7) {
+                set_PWM(523.25);
+            }
+        }
+    }
+    
+    PWM_off();
+    return 0;
+}
