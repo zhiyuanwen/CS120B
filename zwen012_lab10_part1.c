3229ccabb35c83cc5dee0c2349b0cccbc577e371
zhiyuanwen <72575234+zhiyuanwen@users.noreply.github.com>
2021-11-03 23:04:30 -0700

    Add files via upload

--- zwen012_lab10_part1.c
+++ zwen012_lab10_part1.c
@@ -10,9 +10,45 @@
  */
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
+
+volatile unsigned char TimerFlag = 0;
+
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
+void TimerOn() {
+    TCCR1B = 0x0B;
+    OCR1A = 125;
+    TIMSK1 = 0x02;
+    TCNT1 = 0;
+    _avr_timer_cntcurr = _avr_timer_M;
+    SREG |= 0x80;
+}
+
+void TimerOff() {
+    TCCR1B = 0x00;
+}
+
+void TimerISR() {
+    TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect) {
+    _avr_timer_cntcurr--;
+    if(_avr_timer_cntcurr == 0) {
+        TimerISR();
+        _avr_timer_cntcurr = _avr_timer_M;
+    }
+}
+
+void TimerSet(unsigned long M) {
+    _avr_timer_M = M;
+    _avr_timer_cntcurr = _avr_timer_M;
+}
 
 enum led_States { led_s0, led_ s1, led_s2 } led_State;
 
@@ -87,10 +123,14 @@ void CombineLEDsSM() {
 int main(void) {
     DDRA = 0x00; PORTA = 0xFF;
     DDRB = 0xFF; PORTB = 0x00;
+    TimerSet(1000);
+    TimerOn();
     while(1) {
         ThreeLEDsSM();
         BlinkingLEDSM();
         CombineLEDsSM();
+        while(!TimerFlag);
+        TimerFlag = 0;
     }
     return 0;
 }
