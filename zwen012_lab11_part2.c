c11a9695a59b4d029348b6e6cbc900067a01a104
zhiyuanwen <72575234+zhiyuanwen@users.noreply.github.com>
2021-11-14 23:23:32 -0800

    Add files via upload

--- zwen012_lab11_part2.c
+++ zwen012_lab11_part2.c
@@ -17,14 +17,16 @@
 #include "simAVRHeader.h"
 #endif
 
-const char* str1 = "                CS120B is Legend...wait for it DARY!                ";
-char* str2 = "";
-unsigned char i = 0;
-
-enum display_Text { display_p1 };
+enum display_Text { display_p1, display_p2, display_p3 };
 int displaySMText(int state) {
     switch(state) {
         case display_p1:
+            state = display_p2;
+            break;
+        case display_p2:
+            state = display_p3;
+            break;
+        case display_p3:
             state = display_p1;
             break;
         default:
@@ -33,12 +35,13 @@ int displaySMText(int state) {
     }
     switch(state) {
         case display_p1:
-            str2 = str1[i];
-            for(unsigned char j = i + 1; j < i + 16; j++) {
-                str2 = str2 + str1[j];
-            }
-            LCD_DisplayString(1, str2);
-            i = (i + 1) % 68;
+            LCD_DisplayString(1, "CS120B is Legend")
+            break;
+        case display_p2:
+            LCD_DisplayString(1, "wait for it...")
+            break;
+        case display_p3:
+            LCD_DisplayString(1, "DARY!")
             break;
         default:
             break;
@@ -50,31 +53,27 @@ int main(void) {
     DDRA = 0x00; PORTA = 0xFF;
     DDRB = 0xFF; PORTB = 0x00;
     DDRC = 0x00; PORTC = 0xFF;
+    DDRD = 0xFF; PORD = 0x00;
     
     static task task1;
     task *tasks[] = { &task1 };
-    const unsigned short numTasks = sizeof(tasks) / sizeof(task*);
 
     task1.state = -1;
-    task1.period = 100;
+    task1.period = 5000;
     task1.elapsedTime = task1.period;
     task1.TickFct = &displaySMText;
 
-    unsigned short i;
-    unsigned long GCD = tasks[0] -> period;
-    for(i = 1; i < numTasks; i++) {
-        GCD = findGCD(GCD, tasks[i] -> period);
-    }
-    TimerSet(GCD);
+    TimerSet(5000);
     TimerOn();
     LCD_init();
+    LCD_ClearScreen();
     while(1) {
         for(i = 0; i < numTasks; ++i) {
             if(tasks[i] -> elapsedTime == tasks[i] -> period) {
                 tasks[i] -> state = tasks[i] -> TickFct(tasks[i] -> state);
                 tasks[i] -> elapsedTime = 0;
             }
-            tasks[i] -> elapsedTime += GCD;
+            tasks[i] -> elapsedTime += 5000;
         }
         while(!TimerFlag);
         TimerFlag = 0;
