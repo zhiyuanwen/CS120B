1f34086599be2e638ed628adc83987b3be66dbe3
zhiyuanwen <72575234+zhiyuanwen@users.noreply.github.com>
2021-11-14 19:47:24 -0800

    Add files via upload

--- zwen012_lab11_part2.c
+++ zwen012_lab11_part2.c
@@ -1,6 +1,6 @@
 /*	Author: Zhiyuan Wen
  *	Lab Section: 021
- *	Assignment: Lab #11  Exercise #1
+ *	Assignment: Lab #11  Exercise #2
  *	Exercise Description:
  *	
  *	I acknowledge all content contained herein, excluding template 
@@ -18,62 +18,35 @@
 #include "simAVRHeader.h"
 #endif
 
-enum keypad_States { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, *, #, \0};
-int toggleKeypadSMState(int state) {
+char str1[] = "CS120B is Legend... wait for it DARY!";
+unsigned char i = 0;
+unsigned str2[] = "";
+
+enum display_States { display_display };
+int displaySMTick(int state) {
+    unsigned char output;
     switch(state) {
-        case 0:
-            PORTB = 0x00;
-            break;
-        case 1:
-            PORTB = 0x01;
-            break;
-        case 2:
-            PORTB = 0x02;
-            break;
-        case 3:
-            PORTB = 0x03;
-            break;
-        case 4:
-            PORTB = 0x04;
-            break;
-        case 5:
-            PORTB = 0x05;
-            break;
-        case 6:
-            PORTB = 0x06;
-            break;
-        case 7:
-            PORTB = 0x07;
-            break;
-        case 8:
-            PORTB = 0x08;
-            break;
-        case 9:
-            PORTB = 0x09;
-            break;
-        case A:
-            PORTB = 0x0A;
-            break;
-        case B:
-            PORTB = 0x0B;
-            break;
-        case C:
-            PORTB = 0x0C;
-            break;
-        case D:
-            PORTB = 0x0D;
-            break;
-        case *:
-            PORTB = 0x0E;
-            break;
-        case #:
-            PORTB = 0x0F;
-            break;
-        case \0:
-            PORTB = 0x1F;
+        case display_display:
+            state = display_display;
             break;
         default:
-            PORTB = 0x1B;
+            state = display_display;
+            break;
+    }
+    switch(state) {
+        case display_display:
+            LCD_ClearScreen();
+            if(i >= sizeof(str1)) {
+                str2 += str1[sizeof(str1) - i];
+                i--;
+            }
+            else {
+                str2 += str1[i];
+                i++;
+            }
+            LCD_DisplayString(str2);
+            break;
+        default:
             break;
     }
     return state;
@@ -87,10 +60,10 @@ int main(void) {
     _task *tasks[] = { &task1 };
     const unsigned short numTasks = sizeof(tasks) / sizeof(task*);
 
-    task1.state = GetKeypadKey();
-    task1.period = 10;
+    task1.state = display_display;
+    task1.period = 100;
     task1.elapsedTime = task1.period;
-    task1.TickFct = &toggleKeypadSMState;
+    task1.TickFct = &displaySMTick;
 
     unsigned short i;
     unsigned long GCD =  tasks[0] -> period;
